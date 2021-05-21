57a410cdbb6c2bf842a4a378fd62675db758acaf
RAIT ACM Student Chapter <83773945+rait-acm@users.noreply.github.com>
2021-05-21 15:42:10 +0530

    Add files via upload
    All the unsorted pull request are uploaded here

---
+++ Week1/Week_1_unsorted/Purva kharsambale(SE)/main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test Show</title>
+  </head>
+  <body>
+    <fieldset>
+      <div id='Dimensions'>
+        <h3>Submitted Information</h3>
+        <p>First Name: <?php echo $_POST['first_name'];?></p>
+        <p>Last Name: <?php echo $_POST['last_name'];?></p>
+        <p>
+    This is the best day.
+
+  </body>
+</html>
