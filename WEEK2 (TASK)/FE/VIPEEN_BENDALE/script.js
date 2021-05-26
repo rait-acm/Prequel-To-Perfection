e0c3abe81c3ec98aaeee28bc6ecbcecb4d55e0ed
vipeenb <74306253+vipeenb@users.noreply.github.com>
2021-05-26 19:38:16 +0530

    VIPEEN_BENDALE - Background Color Generator

---
+++ WEEK2 (TASK)/FE/VIPEEN_BENDALE/script.js
@@ -0,0 +1,13 @@
+function getNewBgColor() {
+    var symbols,color;
+    symbols = "0123456789ABCDEF";
+
+
+    color = "#";
+    for(var i = 0;i<6;i++) {
+        color=color+symbols[Math.floor(Math.random()*16)];
+    }
+    document.body.style.background=color;
+    document.getElementById("hex").innerHTML = color;
+
+}
